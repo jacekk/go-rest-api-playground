58f6657ca1159910522d6db428c093fd9e3dfb7a
jacekk <github.com@jacek-k.net>
2019-04-16 23:47:09 +0200

    fixed some simple endpoints;

--- internal/routing/router.go
+++ internal/routing/router.go
@@ -14,12 +14,12 @@ func setupRouter() *gin.Engine {
 	simple := router.Group("/simple")
 	{
 		simple.GET("/json", routes.GetQuery)
-		simple.GET("/simple/plain", routes.GetPlainText)
-		simple.GET("/simple/uno/:dos/*tres", routes.GetParams)
+		simple.GET("/plain", routes.GetPlainText)
+		simple.GET("/uno/:dos/*tres", routes.GetParams)
 
-		simple.POST("/simple/json", routes.PostJson)
-		simple.POST("/simple/xml", routes.PostXml)
-		simple.POST("/simple/yml", routes.PostYml)
+		simple.POST("/json", routes.PostJson)
+		simple.POST("/xml", routes.PostXml)
+		simple.POST("/yml", routes.PostYml)
 	}
 	posts := router.Group("/posts")
 	{
